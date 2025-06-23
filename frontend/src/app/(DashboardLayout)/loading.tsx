e55f2b170bc50c5e95e9b02549e8da93c5e96452
Yoni1999 <jonathan.molina@alu.ucm.cl>
2025-06-23 16:49:58 -0400

    lunes 23-06:16:49

--- frontend/src/app/(DashboardLayout)/loading.tsx
+++ frontend/src/app/(DashboardLayout)/loading.tsx
@@ -1,7 +1,49 @@
-const Loading = () =>{
-    return(
-        <div>Cargando Datos</div>
-    )
-}
+'use client';
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Loading = () => {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+      }}
+    >
+      <Box
+        component="img"
+        src="/images/logos/logoitem.jpg"
+        alt="Logo Mimbral"
+        sx={{
+          width: 70, // Más pequeño
+          height: 70,
+          borderRadius: '50%',
+          objectFit: 'cover',
+          animation: 'spin 2s linear infinite',
+          mb: 2, // margen inferior
+        }}
+      />
+      <Typography variant="body1" color="text.secondary" fontWeight={500}>
+        Cargando...
+      </Typography>
+
+      <style jsx global>{`
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </Box>
+  );
+};
 
 export default Loading;
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://192.168.0.115:3001";
+//export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://192.168.0.115:3001";
 
 export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6Lcj9FYrAAAAAElU-1aSh3BtUiEksGEk4y9IJnul";
 
-//export const BACKEND_URL = "https://backmimbral2025.loclx.io";
+export const BACKEND_URL = "https://backmimbral2025.loclx.io";
