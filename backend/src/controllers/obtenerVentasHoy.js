af3657d9f245797572f0f448d0c059e6af416f95
Yoni1999 <jonathan.molina@alu.ucm.cl>
2025-10-01 12:25:46 -0300

    comas corregidas

--- backend/src/controllers/obtenerVentasHoy.js
+++ backend/src/controllers/obtenerVentasHoy.js
@@ -311,7 +311,7 @@ const obtenerVentasPeriodo = async (req, res) => {
                         OR (@CanalParam = 'Balmaceda' AND T1.WhsCode = '07')
                         OR (@CanalParam = 'Vitex' AND T1.WhsCode = '01' AND T1.SlpCode IN (401, 397))
                         OR (@CanalParam = 'Chorrillo' AND T1.WhsCode = '01' 
-                            AND T1.SlpCode NOT IN (401, 397, 355, 398, 227, 250,,225, 205, 138, 209, 228, 226, 137, 212))
+                            AND T1.SlpCode NOT IN (401, 397, 355, 398, 227, 250,225, 205, 138, 209, 228, 226, 137, 212))
                         OR (@CanalParam = 'Empresas' AND T1.WhsCode = '01' 
                             AND T1.SlpCode IN (227, 250, 225, 205, 138, 209, 228, 226, 137, 212))
                     )
@@ -434,7 +434,7 @@ const obtenerTransaccionesPeriodo = async (req, res) => {
                         OR (@CanalParam = 'Chorrillo' AND T1.WhsCode = '01' 
                             AND T1.SlpCode NOT IN (401, 397, 355, 398, 227, 250, 205, 225, 138, 209, 228, 226, 137, 212))
                         OR (@CanalParam = 'Empresas' AND T1.WhsCode = '01' 
-                            AND T1.SlpCode IN (227, 250, 205, 138,, 225 209, 228, 226, 137, 212))
+                            AND T1.SlpCode IN (227, 250, 205, 138,, 225, 209, 228, 226, 137, 212))
                     )
                 )
                 AND (@VendedorEmpresaParam IS NULL OR T1.SlpCode = @VendedorEmpresaParam)
